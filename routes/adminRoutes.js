bec40273e146c1aac4280cf64a1a4858416bf0f9
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-09-25 21:55:01 +0600

    add admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -15,7 +15,7 @@ const { checkLogin } = require('../middleware/checkLogin');
 const router = express.Router();
 
 // routes
-router.post('/', checkLogin, adminCreate);
+router.post('/', adminCreate);
 
 router.post('/login', adminLogin);
 
