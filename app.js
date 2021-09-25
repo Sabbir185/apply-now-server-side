78f6758e98b0456147a058b937c3bc70f5dc6f53
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-09-25 20:03:23 +0600

    server push

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // external import
 const express = require('express');
 const dotenv = require('dotenv');
-const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 
@@ -18,10 +17,6 @@ dotenv.config();
 
 
 // middleware
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
-
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
